fd9fe787c47b0ded1667b00a20d995510ba9bc23
Vlad1sl4v <122152653+h4ze-07@users.noreply.github.com>
2024-05-07 20:55:15 +0300

    day 1

--- lesson01/src/App.java
+++ lesson01/src/App.java
@@ -1,8 +1,66 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String greeting = "Hello world";
+        //VARIABLES
+        // byte x = 127;
+        // int y = 250;
+        // boolean res = x < y;
+        // short sNum = 32767;
+        // long longNum = 123263627674L;
+        
+        // float fNum = 3.1415f;
+        // double dNum = 3.1415161718;
 
-        System.out.println(greeting);
+        // char W = 'W';
+        // String greeting = "Hello world";
+
+        // System.out.println("Max value: " +  x);
+        // System.out.println(y);
+        // System.out.println(res);
+        // System.out.println(sNum);
+        // System.out.println(longNum);
+        // System.out.println(fNum);
+        // System.out.println(dNum);
+        // System.out.println(W);
+        // System.out.println(greeting);
+        
+        //Swapping to variables
+        // char x = 'x';
+        // char y = 'y';
+
+        // System.out.println("x: " + x);
+        // System.out.println("y: " + y);
+
+        // char temp;
+
+        // temp = x;
+        // x = y;
+        // y = temp;
+
+        // System.out.println("x: " + x);
+        // System.out.println("y: " + y);
+
+        //USER INPUT
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("What is your name: ");
+        // String name = scanner.nextLine();
+
+        // System.out.println("Age: ");
+        // int age = scanner.nextInt();
+
+        // System.out.print(name);
+        // System.out.print(", ");
+        // System.out.print(age);
+
+        //EXPRESSIONS
+        int friends = 10;
+        System.out.println(friends + 2);
+        System.out.println(friends - 3);
+        System.out.println(friends * 2);
+        System.out.println(friends / 5);
+        System.out.println(friends % 2);
     }
 }
